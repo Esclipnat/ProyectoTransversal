97589726738ec007b0f8d9c5db6c2d98a331d504
Cristian Garcia <cristian_garcia@outlook.com>
2023-09-07 12:02:35 -0300

    Update Conexion Java
    Cambiamos un poco el url

--- src/proyectotransversal/AccesoAData/Conexion.java
+++ src/proyectotransversal/AccesoAData/Conexion.java
@@ -7,30 +7,32 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
 
-    private static final String url="jdbc:mariadb://localhost/";
-    private static final String DB="ProyectoTransversal";
-    private static final String usuario="root";
-    private static String clave="";
-            
-    private static Conexion conexion=null;
-    
-    private Conexion (){
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "proyectotransversal";
+    private static final String USUARIO = "root";
+    private static String PASSWORD = "";
+
+    private static Conexion conexion = null;
+
+    private Conexion() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "No se puede establecer la conexion [" +ex.getMessage()+"]");
+            JOptionPane.showMessageDialog(null, "No se puede establecer la conexion [" + ex.getMessage() + "]");
         }
-        
+
     }
-    public static Connection getConexion(){
-        Connection con=null;
-        if(conexion==null){
-            conexion=new Conexion();
+
+    public static Connection getConexion() {
+        Connection con = null;
+        if (conexion == null) {
+            conexion = new Conexion();
         }
         try {
-            con=DriverManager.getConnection(url+DB,usuario,clave);
-                    } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error de conexion [" +ex.getMessage()+"]");
+            con = DriverManager.getConnection(URL + DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                    + "&user=" + USUARIO + "&password=" + PASSWORD);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion [" + ex.getMessage() + "]");
         }
         return con;
     }
