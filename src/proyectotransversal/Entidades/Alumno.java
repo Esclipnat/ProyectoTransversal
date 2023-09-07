30a43ad6fb24339b0e77de969e57479c2a28b003
145456as <agus.play.lazcano@gmail.com>
2023-09-07 11:33:35 -0300

    correccion de creacion alumno

--- src/proyectotransversal/Entidades/Alumno.java
+++ src/proyectotransversal/Entidades/Alumno.java
@@ -1,14 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 package proyectotransversal.Entidades;
 
-/**
- *
- * @author Nahuel
- */
-public class Alumno {
+import java.util.Date;
 
+public class Alumno {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private boolean activo;
+
+    public Alumno(int id, String nombre, String apellido, Date fechaNacimiento, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String toString() {
+        return "Alumno{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", activo=" + activo +
+                '}';
+    }
 }
